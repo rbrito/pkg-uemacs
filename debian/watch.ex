cb9c91ace08d88415313c8767da431a9d338d280
Rogério Brito <rbrito@ime.usp.br>
2021-02-21 22:58:55 -0300

    debian: Remove example files.

--- debian/watch.ex
+++
@@ -1,5 +0,0 @@
-version=4
-
-opts="filenamemangle=s%(?:.*?)?v?(\d[\d.]*)\.tar\.gz%uemacs-$1.tar.gz%" \
-   https://github.com/torvalds/uemacs/tags \
-   (?:.*?/)?v?(\d[\d.]*)\.tar\.gz debian uupdate
